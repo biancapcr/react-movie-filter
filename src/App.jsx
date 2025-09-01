c1dc0bbb244f204dbebd65b8fb0750945a357188
biancapcr <biancacristina.pocora@gmail.com>
2025-09-01 17:15:03 +0200

    [feat](app): integrate results section and genre selector

--- src/App.jsx
+++ src/App.jsx
@@ -16,50 +16,49 @@ const initialMovies = [
   const [sourceMovies] = useState(initialMovies);
 
   // tracciamento filtro selezionato
-  const [genreFilter, setGenreFilter] = useState(""); 
+  const [genreFilter, setGenreFilter] = useState("");
 
   // mantenimento risultati da mostrare in UI
-  const [results, setResults] = useState(initialMovies); 
+  const [results, setResults] = useState(initialMovies);
 
   // ricavo lista dei generi disponibili dai dati
   const genres = ["", ...Array.from(new Set(sourceMovies.map(m => m.genre))).sort()];
 
   // ricalcolo risultato quando cambia il filtro
-  seEffect(() => {
+  useEffect(() => {
     // applicazione filtro & aggiornamento stato
-  setResults(
-    sourceMovies.filter(
-      (movie) => genreFilter === "" || movie.genre === genreFilter
-    )
-  );
-}, [genreFilter, sourceMovies]);
-
-
+    setResults(
+      sourceMovies.filter(
+        (movie) => genreFilter === "" || movie.genre === genreFilter
+      )
+    );
+  }, [genreFilter, sourceMovies]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <h1>Seleziona Film per Genere</h1>
 
-export default App
+      {/* menu*/}
+      <select
+        value={genreFilter}
+        onChange={(e) => setGenreFilter(e.target.value)}
+      >
+        <option value="">Seleziona Genere</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+
+      {/* lista */}
+      <ul>
+        {results.map((movie, index) => (
+          <li key={index}>
+            {movie.title} - <i>{movie.genre}</i>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
