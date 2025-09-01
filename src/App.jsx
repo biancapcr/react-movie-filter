cd5cdcaddaa8d518b259339e87d982ef6eb1a10f
biancapcr <biancacristina.pocora@gmail.com>
2025-09-01 16:55:04 +0200

    [feat] (filter) add dynamic genre filtering with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// importazione
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// dati iniziali
+const initialMovies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+
+  function App() {
+  // sorgente
+  const [sourceMovies] = useState(initialMovies);
+
+  // tracciamento filtro selezionato
+  const [genreFilter, setGenreFilter] = useState(""); 
+
+  // mantenimento risultati da mostrare in UI
+  const [results, setResults] = useState(initialMovies); 
+
+  // ricavo lista dei generi disponibili dai dati
+  const genres = ["", ...Array.from(new Set(sourceMovies.map(m => m.genre))).sort()];
+
+  // ricalcolo risultato quando cambia il filtro
+  seEffect(() => {
+    // applicazione filtro & aggiornamento stato
+  setResults(
+    sourceMovies.filter(
+      (movie) => genreFilter === "" || movie.genre === genreFilter
+    )
+  );
+}, [genreFilter, sourceMovies]);
+
+
 
   return (
     <>
